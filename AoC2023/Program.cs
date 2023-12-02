ef4a76af6b258d647c7b9c061519b785d1fdd464
Vitaliy Roshchupkin <vitalya.cookie@gmail.com>
2023-12-02 20:45:24 +0600

    giga refactoring

--- AoC2023/Program.cs
+++ AoC2023/Program.cs
@@ -1,49 +1,11 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Main
+﻿namespace AoC2023
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var regex = new Regex("(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))", RegexOptions.Compiled);
-            var map = new Dictionary<string, ulong>
-            {
-                ["one"] = 1,
-                ["two"] = 2,
-                ["three"] = 3,
-                ["four"] = 4,
-                ["five"] = 5,
-                ["six"] = 6,
-                ["seven"] = 7,
-                ["eight"] = 8,
-                ["nine"] = 9,
-            };
-
-            ulong result = 0;
-            var line = Console.ReadLine();
-            while (line != "")
-            {
-                var matches = regex.Matches(line);
-                var first = matches.First().Groups.Values.Last().Value;
-                var last = matches.Last().Groups.Values.Last().Value;
-
-                var res = new[] { last, first }.ToArray();
-
-                for (var i = 1; i >= 0; i--)
-                {
-                    result += (ulong)Math.Pow(10, i) * Parse(res[i]);
-                }
-
-                line = Console.ReadLine();
-
-                ulong Parse(string str)
-                {
-                    return str.Length == 1 ? ulong.Parse(str) : map[str];
-                }
-            }
-
-            Console.WriteLine(result);
+            Task1.Solve();
         }
+        
     }
 }
